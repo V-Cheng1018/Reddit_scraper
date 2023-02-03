469fb1bffc0b8c65856c6753b8e5b302cf568986
V-Cheng1018 <63609469+V-Cheng1018@users.noreply.github.com>
2023-02-03 01:30:58 -0500

    Update serach_wsb.py

--- serach_wsb.py
+++ serach_wsb.py
@@ -4,16 +4,16 @@ import datetime
 import psycopg2
 import psycopg2.extras
 
-# connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
-# cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-# cursor.execute("""
-#     SELECT * FROM stock
-# """)
-# rows = cursor.fetchall()
+connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cursor.execute("""
+    SELECT * FROM stock
+""")
+rows = cursor.fetchall()
 
-# stocks = {}
-# for row in rows: 
-#     stocks['$' + row['symbol']] = row['id']
+stocks = {}
+for row in rows: 
+    stocks['$' + row['symbol']] = row['id']
 
 api = PushshiftAPI()
 
@@ -23,26 +23,26 @@ submissions = list(api.serach_submissions(after=start_time,
                         subreddit="wallstreetbet s",
                         filter=['url','author', 'title', 'subreddit'],
                         limit=10))
-print(submissions)
-# for submission in submissions:
-#     words = submission.title.split()
-#     cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
 
-#     if len(cashtags) > 0:
-#         print(cashtags)
-#         print(submission.title)
+for submission in submissions:
+    words = submission.title.split()
+    cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
 
-#         for cashtag in cashtags:
-#             if cashtag in stocks:
-#                 submitted_time = datetime.datetime.fromtimestamp(submission.created_utc).isoformat()
+    if len(cashtags) > 0:
+        print(cashtags)
+        print(submission.title)
 
-#                 try:
-#                     cursor.execute("""
-#                         INSERT INTO mention (dt, stock_id, message, source, url)
-#                         VALUES (%s, %s, %s, 'wallstreetbets', %s)
-#                     """, (submitted_time, stocks[cashtag], submission.title, submission.url))
+        for cashtag in cashtags:
+            if cashtag in stocks:
+                submitted_time = datetime.datetime.fromtimestamp(submission.created_utc).isoformat()
 
-#                     connection.commit()
-#                 except Exception as e:
-#                     print(e)
-#                     connection.rollback()
+                try:
+                    cursor.execute("""
+                        INSERT INTO mention (dt, stock_id, message, source, url)
+                        VALUES (%s, %s, %s, 'wallstreetbets', %s)
+                    """, (submitted_time, stocks[cashtag], submission.title, submission.url))
+
+                    connection.commit()
+                except Exception as e:
+                    print(e)
+                    connection.rollback()
