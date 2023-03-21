98c5122e26714edfeffe2d6cc7966217f6b8289e
V-Cheng1018 <63609469+V-Cheng1018@users.noreply.github.com>
2023-03-21 13:23:18 -0400

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-API_URL = 'https://api.alpaca.markets'
-API_KEY = 'AKRSALEI4G38P2JEN4V0'
-API_SECRET = '7NKApM0hRTfxTOPOA68PDIjqQ5lid5bLU7Bexh02'
-
-
-DB_HOST = 'localhost'
-DB_USER = 'postgres'
-DB_PASS = 'password'
-DB_NAME = 'etfdb'
